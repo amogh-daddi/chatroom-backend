7e5da6d1d63eee43b3177fb569c960c7180e5aff
Amogh <amoghsdaddi@gmail.com>
2023-08-26 23:50:00 +0530

    changes done in message file.

--- src/main/java/com/socketio/backend/model/Message.java
+++ src/main/java/com/socketio/backend/model/Message.java
@@ -1,12 +1,6 @@
 package com.socketio.backend.model;
 
-import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@Data
-@Builder
 public class Message {
 
     private MessageType messageType;
@@ -16,5 +10,45 @@ public class Message {
 
     private String username;
 
+    public Message() {
+    }
 
+    public Message(MessageType messageType, String content, String room, String username) {
+        this.messageType = messageType;
+        this.content = content;
+        this.room = room;
+        this.username = username;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
