a073093a49f4f2a3246293f1e4f88909342e6d23
Sneha <snehabannuri1999@gmail.com>
2023-12-24 23:19:57 +0530

    Added autowired in ServerCommandLineRunner.java

--- src/main/java/com/socketio/backend/config/ServerCommandLineRunner.java
+++ src/main/java/com/socketio/backend/config/ServerCommandLineRunner.java
@@ -3,6 +3,7 @@ package com.socketio.backend.config;
 import com.corundumstudio.socketio.SocketIOServer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 public class ServerCommandLineRunner implements CommandLineRunner {
-    private final SocketIOServer server;
+    @Autowired
+    private SocketIOServer server;
 
     @Override
     public void run(String... args) throws Exception {
